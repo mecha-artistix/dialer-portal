afa24262dbc96173599ce2c9d202b181a080765f
huzaifa <mecha.artistix@gmail.com>
2024-11-13 22:57:17 +0500

    styling

--- src/features/recordings/components/ApiQueryForm.tsx
+++ src/features/recordings/components/ApiQueryForm.tsx
@@ -83,7 +83,7 @@ function ApiQueryForm() {
       <ServerResponse type={status.status} message={status.message} />
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-end">
             {/* DIALER URL */}
             <FormField
               control={form.control}
@@ -160,11 +160,11 @@ function ApiQueryForm() {
             />
 
             {/* SUBMIT BUTTON */}
-            <div className="flex items-center justify-center">
-              <Button type="submit" disabled={isSubmitting}>
-                {isSubmitting ? "Fetching Data" : "Submit"}
-              </Button>
-            </div>
+            {/* <div className="flex items-center justify-center"> */}
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Fetching Data" : "Submit"}
+            </Button>
+            {/* </div> */}
           </div>
         </form>
       </Form>

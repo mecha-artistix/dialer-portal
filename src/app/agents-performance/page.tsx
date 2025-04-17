def16fda8ec953474f26f9c22013ebc686d83426
huzaifa <mecha.artistix@gmail.com>
2025-04-17 11:05:25 +0300

    agent performace

--- src/app/agents-performance/page.tsx
+++ src/app/agents-performance/page.tsx
@@ -1,6 +1,59 @@
 import axios from "axios";
 import Agent from "./components/Agent";
 
+const staticData = [
+  {
+    time: "2025-04-16T11:41:46.000Z",
+    user: "10021",
+    lead_id: 10146348,
+    phone_number: "9489990785",
+    Duration: null,
+    campaign_id: "NewBot",
+    comments: "Chelsea",
+    status: "DNQ",
+  },
+  {
+    time: "2025-03-31T18:07:32.000Z",
+    user: "10027",
+    lead_id: 10146344,
+    phone_number: "9489007700",
+    Duration: 17,
+    campaign_id: "NewBot",
+    comments: "Olivia",
+    status: "DAIR",
+  },
+  {
+    time: "2025-04-16T14:36:55.000Z",
+    user: "10060",
+    lead_id: 10146344,
+    phone_number: "9489007700",
+    Duration: 31,
+    campaign_id: "NewBot",
+    comments: "Olivia",
+    status: "NP",
+  },
+  {
+    time: "2025-04-03T17:15:42.000Z",
+    user: "10039",
+    lead_id: 10146341,
+    phone_number: "9489007316",
+    Duration: 7,
+    campaign_id: "NewBot",
+    comments: "Chris",
+    status: "DAIR",
+  },
+  {
+    time: "2025-04-08T14:33:16.000Z",
+    user: "10039",
+    lead_id: 10146341,
+    phone_number: "9489007316",
+    Duration: 17,
+    campaign_id: "NewBot",
+    comments: "Chris",
+    status: "DAIR",
+  },
+];
+
 async function page() {
   let data = [];
   try {

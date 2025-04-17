fddaa139e4a4e2fd2a37d96383e17aa2adbcac59
huzaifa <mecha.artistix@gmail.com>
2025-04-17 10:55:55 +0300

    agent performace

--- src/app/agents-performance/page.tsx
+++ src/app/agents-performance/page.tsx
@@ -1,10 +1,12 @@
 import axios from "axios";
+import Agent from "./components/Agent";
 
 async function page() {
   let data = {};
   try {
     const res = await axios.post("http://localhost:9898/api/agents-performance");
-    data = res.data;
+    data = res.data.data;
+    console.log(data);
   } catch (error) {
     console.error("Error fetching data:", error);
     data = { error };
@@ -13,6 +15,20 @@ async function page() {
   return (
     <div>
       <p>Agents Performance</p>
+      <div>
+        {data?.map((row) => (
+          <div key={row.lead_id}>
+            <Agent />
+            <p>{row.user}</p>
+            <p>{row.lead_id}</p>
+            <p>{row.phone_number}</p>
+            <p>{row.Duration}</p>
+            <p>{row.campaign_id}</p>
+            <p>{row.comments}</p>
+            <p>{row.status}</p>
+          </div>
+        ))}
+      </div>
       <div>{JSON.stringify(data)}</div>
     </div>
   );
